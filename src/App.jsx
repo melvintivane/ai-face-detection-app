76b07ec7d17cd4f4c5832ec80028164ccebf5518
melvintivane <melvinshuster47@gmail.com>
2023-10-04 18:21:29 +0200

    Add Navbar component and image posting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import NewPost from "./components/post/NewPost";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
 
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        setImage({
+          url: img.src,
+          width: img.width,
+          height: img.height,
+        });
+      };
+    };
+
+    file && getImage();
+  }, [file]);
+
+  console.log(image);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navbar />
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src="https://images.pexels.com/photos/1321909/pexels-photo-1321909.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              className="avatar"
+            />
+            <div className="inputForm">
+              <input
+                type="text"
+                placeholder="What's on your mind?"
+                className="postInput"
+              />
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                  alt=""
+                />
+                <button>Send</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
