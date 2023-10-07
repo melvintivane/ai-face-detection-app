af1cc5ebb30f8d88adb76384949f77f16582449d
melvintivane <melvinshuster47@gmail.com>
2023-10-07 08:05:13 +0200

    Add detected face count and feedback sending functionality

--- src/components/post/NewPost.jsx
+++ src/components/post/NewPost.jsx
@@ -4,6 +4,7 @@ import * as faceapi from "face-api.js";
 const NewPost = ({ image }) => {
   const { url, width, height } = image;
   const [isLoading, setIsLoading] = useState(false);
+  const [count, setCount] = useState(0);
   const imgRef = useRef();
   const canvasRef = useRef();
 
@@ -15,19 +16,17 @@ const NewPost = ({ image }) => {
 
     // Cria um canvas a partir da imagem usando a biblioteca faceapi.
     canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
-
     // Define as dimensões do canvas para corresponder às dimensões da imagem.
     faceapi.matchDimensions(canvasRef.current, {
       width,
       height,
     });
-
+    setCount(detections.length);
     // Redimensiona os resultados da detecção para coincidir com as dimensões da imagem.
     const resized = faceapi.resizeResults(detections, {
       width,
       height,
     });
-
     // Desenha as detecções, expressões e pontos de referência faciais no canvas
     faceapi.draw.drawDetections(canvasRef.current, resized);
     faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
@@ -53,6 +52,20 @@ const NewPost = ({ image }) => {
     }, 1000);
   }, [isLoading]);
 
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const inputElement = document.querySelector(".rightInput");
+    const inputValue = inputElement.value.trim();
+
+    if (inputValue) {
+      window.alert("Feedback Sent!!");
+      window.location.reload();
+    } else {
+      alert("Please enter feedback before submitting.");
+    }
+  };
+
   return (
     <div className="wrapper">
       {isLoading ? (
@@ -67,16 +80,24 @@ const NewPost = ({ image }) => {
           <canvas ref={canvasRef} className="canva" />
         </div>
       ) : (
-        <div class="left skeleton-km971twiw4p"></div>
+        <div className="left skeleton-km971twiw4p"></div>
       )}
       <div className="right">
-        <h1>Share your post</h1>
+        {count == 0 ? (
+          <h1>Loading...</h1>
+        ) : (
+          <h1>
+            {count} {count > 1 ? "Faces" : "Face"} <br/>Detected
+          </h1>
+        )}
         <input
           type="text"
-          placeholder="What's on your mind?"
+          placeholder="Have any feeback?"
           className="rightInput"
         />
-        <button className="rightButton">Send</button>
+        <button type="submit" onClick={handleClick} className="button">
+          Tell us
+        </button>
       </div>
     </div>
   );
